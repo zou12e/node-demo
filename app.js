d45d25db3e9ffe175821229601981095d2cdd197
jeff <281933726@qq.com>
2018-05-08 00:46:58 +0800

    init

--- app.js
+++ app.js
@@ -9,9 +9,20 @@ app.use(express.static('./html'));
 // 使用body-parser 解析
 app.use(bodyParser());
 
+
+app.get('/get/data',function(req, res){
+    console.log('----------------------get----------------------');
+    console.log(req.query);
+    console.log('name', req.query.name);
+    console.log('age', req.query.age);
+    res.status(200).json({code:0});
+});
+
+
 app.post('/post/data',function(req, res){
+    console.log('----------------------post----------------------');
     console.log(req.body);
-    console.log(req.body.name);
-    console.log(req.body.age);
+    console.log('name', req.body.name);
+    console.log('age', req.body.age);
     res.status(200).json({code:0});
 });
